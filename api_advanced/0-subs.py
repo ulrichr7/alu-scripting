fe28dd7ce7b31997ccbcb6d91c8a2e2754034954
ulrichr7 <ulrichskyler7@gmail.com>
2024-07-05 17:45:26 +0000

    pop

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
-
-
-import json
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
-        return 0
+    """
+    number of subscribers
+    """
+    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+        return 0
