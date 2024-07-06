84306e0925236b9ff7adcb84e48190e32dc03064
ulrichr7 <ulrichskyler7@gmail.com>
2024-07-06 14:48:21 +0000

    pop

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     if len(sys.argv) < 2:
         return 0
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        url = "https://api.reddit.com/r/{}/about".format(subreddit)
         headers = {"User-Agent": "Mozilla/5.0"}
         result = requests.get(url, headers=headers, allow_redirects=False)
         if result.status_code != 200:
